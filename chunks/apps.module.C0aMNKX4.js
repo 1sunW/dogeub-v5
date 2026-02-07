b95c34ea7fd413de7fa5a563af1f1be9abb38e52
1sun <149412950+1sunW@users.noreply.github.com>
2026-02-06 21:26:03 -0500

    Create apps.module.C0aMNKX4.js

---
+++ chunks/apps.module.C0aMNKX4.js
@@ -0,0 +1,87 @@
+(function(e, f) {
+    const o = d
+      , g = e();
+    while (!![]) {
+        try {
+            const h = -parseInt(o(0x142)) / 0x1 * (-parseInt(o(0x154)) / 0x2) + -parseInt(o(0x14e)) / 0x3 + -parseInt(o(0x14c)) / 0x4 + -parseInt(o(0x147)) / 0x5 + -parseInt(o(0x143)) / 0x6 * (parseInt(o(0x141)) / 0x7) + parseInt(o(0x150)) / 0x8 + parseInt(o(0x145)) / 0x9;
+            if (h === f)
+                break;
+            else
+                g['push'](g['shift']());
+        } catch (i) {
+            g['push'](g['shift']());
+        }
+    }
+}(c, 0x4b7c1));
+function d(a, b) {
+    a = a - 0x141;
+    const e = c();
+    let f = e[a];
+    return f;
+}
+const b = (function() {
+    let e = !![];
+    return function(f, g) {
+        const h = e ? function() {
+            if (g) {
+                const i = g['apply'](f, arguments);
+                return g = null,
+                i;
+            }
+        }
+        : function() {}
+        ;
+        return e = ![],
+        h;
+    }
+    ;
+}())
+  , a = b(this, function() {
+    const q = d
+      , e = {
+        'IeLya': function(j, k) {
+            return j(k);
+        },
+        'zKaZp': function(j, k) {
+            return j + k;
+        },
+        'QhgFW': q(0x152)
+    }
+      , f = function() {
+        const r = q;
+        let j;
+        try {
+            j = e[r(0x14b)](Function, e['zKaZp'](e['zKaZp']('return\x20(function()\x20', e[r(0x14f)]), ');'))();
+        } catch (k) {
+            j = window;
+        }
+        return j;
+    }
+      , g = f()
+      , h = g['console'] = g[q(0x153)] || {}
+      , i = [q(0x148), q(0x149), q(0x144), 'error', 'exception', q(0x14a), q(0x14d)];
+    for (let j = 0x0; j < i[q(0x146)]; j++) {
+        const k = b['constructor']['prototype']['bind'](b)
+          , l = i[j]
+          , m = h[l] || k;
+        k['__proto__'] = b[q(0x151)](b),
+        k['toString'] = m['toString'][q(0x151)](m),
+        h[l] = k;
+    }
+});
+a();
+const n = 'nkxkdi8\x20eit0mon'
+  , p = 'oske3pj\x20d360yns'
+  , s = {
+    'appContainer': n,
+    'app': p
+};
+function c() {
+    const t = ['warn', 'table', 'IeLya', '2322908kWfOjb', 'trace', '1527996irKXtD', 'QhgFW', '4146064iaijjX', 'bind', '{}.constructor(\x22return\x20this\x22)(\x20)', 'console', '8SaZTtY', '35eKAcRW', '77257EhEUms', '504948bOjiXl', 'info', '12221550sIJleq', 'length', '1826010kJKUgc', 'log'];
+    c = function() {
+        return t;
+    }
+    ;
+    return c();
+}
+export {s};
