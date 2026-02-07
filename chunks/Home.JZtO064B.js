f56cd298b7453b80ba1aa12f8568b1f74f281ea2
1sun <149412950+1sunW@users.noreply.github.com>
2026-02-06 21:24:22 -0500

    Create Home.JZtO064B.js

---
+++ chunks/Home.JZtO064B.js
@@ -0,0 +1,153 @@
+const N = g;
+function f() {
+    const Q = ['log', 'console', 'JIfmr', 'trace', 'span', '2263210RHEWpc', 'jsxs', 'constructor', '5RmuAJC', 'Fragment', 'length', '5744OoWBef', 'http://www.w3.org/2000/svg', 'useState', 'text-gray-500', '_blank', '2257467XaABIS', 'bind', 'svg', 'exception', 'w-full\x20fixed\x20bottom-0\x20flex\x20items-end\x20justify-between\x20p-2', 'flex\x20gap-2\x20items-center', 'siteTextColor', '5247732yIHYfV', 'hover:-translate-y-0.5\x20duration-200', '18810zLtCTH', 'Bookmarks', 'donationBtn', 'Footer', '1386cCujlF', 'path', '{}.constructor(\x22return\x20this\x22)(\x20)', '7106sPNgwo', 'error', 'MWzKb', 'jsx', 'Home', 'info', '/ds', 'prototype', '459dXJGWW', 'return\x20(function()\x20', 'memo', 'bi\x20bi-discord', '37428sINNjo', 'displayName', '1AdUIOF', '632648gYHvUJ'];
+    f = function() {
+        return Q;
+    }
+    ;
+    return f();
+}
+(function(B, C) {
+    const L = g
+      , D = B();
+    while (!![]) {
+        try {
+            const E = -parseInt(L(0x125)) / 0x1 * (parseInt(L(0x12c)) / 0x2) + -parseInt(L(0x137)) / 0x3 + -parseInt(L(0x126)) / 0x4 + parseInt(L(0x12f)) / 0x5 * (parseInt(L(0x13e)) / 0x6) + -parseInt(L(0x144)) / 0x7 * (parseInt(L(0x132)) / 0x8) + parseInt(L(0x14f)) / 0x9 * (-parseInt(L(0x140)) / 0xa) + parseInt(L(0x147)) / 0xb * (parseInt(L(0x123)) / 0xc);
+            if (E === C)
+                break;
+            else
+                D['push'](D['shift']());
+        } catch (F) {
+            D['push'](D['shift']());
+        }
+    }
+}(f, 0x94b79));
+function g(a, b) {
+    a = a - 0x121;
+    const c = f();
+    let d = c[a];
+    return d;
+}
+const e = (function() {
+    let B = !![];
+    return function(C, D) {
+        const E = B ? function() {
+            if (D) {
+                const F = D['apply'](C, arguments);
+                return D = null,
+                F;
+            }
+        }
+        : function() {}
+        ;
+        return B = ![],
+        E;
+    }
+    ;
+}())
+  , b = e(this, function() {
+    const M = g
+      , B = {
+        'KxdjJ': 'warn',
+        'PIDFG': M(0x14c),
+        'QgcvN': M(0x13a),
+        'dKuao': 'table',
+        'YlweN': function(F, G) {
+            return F < G;
+        }
+    };
+    let C;
+    try {
+        const F = Function(M(0x150) + M(0x146) + ');');
+        C = F();
+    } catch (G) {
+        C = window;
+    }
+    const D = C[M(0x128)] = C['console'] || {}
+      , E = [M(0x127), B['KxdjJ'], B['PIDFG'], M(0x148), B['QgcvN'], B['dKuao'], M(0x12a)];
+    for (let H = 0x0; B['YlweN'](H, E[M(0x131)]); H++) {
+        const I = e[M(0x12e)][M(0x14e)][M(0x138)](e)
+          , J = E[H]
+          , K = D[J] || I;
+        I['__proto__'] = e['bind'](e),
+        I['toString'] = K['toString'][M(0x138)](K),
+        D[J] = I;
+    }
+});
+b();
+import {j as i, r as k, b as n, H as q, y as r} from './vendor-modules.DTgpwBwG.js';
+import {N as v} from './Nav.CMWBzhLH.js';
+import {u as w, B as y, S as z, Q as A} from '../Bp0cuWS2.js';
+const h = ({fill: B}) => i['jsx'](N(0x139), {
+    'xmlns': N(0x133),
+    'width': '16',
+    'height': '16',
+    'fill': B,
+    'className': N(0x122),
+    'viewBox': '0\x200\x2016\x2016',
+    'children': i[N(0x14a)](N(0x145), {
+        'd': 'M13.545\x202.907a13.2\x2013.2\x200\x200\x200-3.257-1.011.05.05\x200\x200\x200-.052.025c-.141.25-.297.577-.406.833a12.2\x2012.2\x200\x200\x200-3.658\x200\x208\x208\x200\x200\x200-.412-.833.05.05\x200\x200\x200-.052-.025c-1.125.194-2.22.534-3.257\x201.011a.04.04\x200\x200\x200-.021.018C.356\x206.024-.213\x209.047.066\x2012.032q.003.022.021.037a13.3\x2013.3\x200\x200\x200\x203.995\x202.02.05.05\x200\x200\x200\x20.056-.019q.463-.63.818-1.329a.05.05\x200\x200\x200-.01-.059l-.018-.011a9\x209\x200\x200\x201-1.248-.595.05.05\x200\x200\x201-.02-.066l.015-.019q.127-.095.248-.195a.05.05\x200\x200\x201\x20.051-.007c2.619\x201.196\x205.454\x201.196\x208.041\x200a.05.05\x200\x200\x201\x20.053.007q.121.1.248.195a.05.05\x200\x200\x201-.004.085\x208\x208\x200\x200\x201-1.249.594.05.05\x200\x200\x200-.03.03.05.05\x200\x200\x200\x20.003.041c.24.465.515.909.817\x201.329a.05.05\x200\x200\x200\x20.056.019\x2013.2\x2013.2\x200\x200\x200\x204.001-2.02.05.05\x200\x200\x200\x20.021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03\x200\x200\x200-.02-.019m-8.198\x207.307c-.789\x200-1.438-.724-1.438-1.612s.637-1.613\x201.438-1.613c.807\x200\x201.45.73\x201.438\x201.613\x200\x20.888-.637\x201.612-1.438\x201.612m5.316\x200c-.788\x200-1.438-.724-1.438-1.612s.637-1.613\x201.438-1.613c.807\x200\x201.451.73\x201.438\x201.613\x200\x20.888-.631\x201.612-1.438\x201.612'
+    })
+})
+  , t = k['memo']( () => {
+    const O = N
+      , B = {
+        'PZdBP': O(0x136),
+        'MWzKb': 'https://ko-fi.com/I3I81MF4CH',
+        'dBqRJ': function(G, H, I) {
+            return G(H, I);
+        },
+        'yyvHt': 'Support\x20us',
+        'tcTJQ': 'div',
+        'QxSDF': O(0x12b),
+        'JIfmr': O(0x13f)
+    }
+      , {options: C} = w()
+      , [D,E] = k[O(0x134)](!0x1)
+      , F = k['useCallback']( () => {
+        const P = O;
+        window['open'](P(0x14d), B['PZdBP']);
+    }
+    , []);
+    return i[O(0x12d)]('div', {
+        'className': O(0x13b),
+        'children': [C[O(0x142)] !== !0x1 && i['jsxs']('a', {
+            'href': B[O(0x149)],
+            'target': '_blank',
+            'rel': 'noopener\x20noreferrer',
+            'className': B['dBqRJ'](n, 'flex\x20gap-1\x20items-center\x20cursor-pointer', 'hover:-translate-y-0.5\x20duration-200'),
+            'children': [i[O(0x14a)](q, {
+                'className': 'w-4'
+            }), B['yyvHt']]
+        }), i['jsxs'](B['tcTJQ'], {
+            'className': O(0x13c),
+            'children': [i['jsxs']('div', {
+                'className': n('flex\x20gap-1\x20items-center\x20cursor-pointer', O(0x13f)),
+                'onClick': F,
+                'children': [i['jsx'](h, {
+                    'className': 'w-4',
+                    'fill': C[O(0x13d)] || '#a0b0c8'
+                }), 'Discord']
+            }), i['jsx'](B['QxSDF'], {
+                'className': O(0x135),
+                'children': '•'
+            }), i[O(0x12d)]('div', {
+                'className': n('flex\x20gap-1\x20items-center\x20cursor-pointer', B[O(0x129)]),
+                'onClick': () => E(!0x0),
+                'children': [i[O(0x14a)](r, {
+                    'className': 'w-4'
+                }), O(0x141)]
+            })]
+        }), i['jsx'](y, {
+            'isOpen': D,
+            'onClose': () => E(!0x1)
+        })]
+    });
+}
+);
+t[N(0x124)] = N(0x143);
+const j = k[N(0x121)]( () => i['jsxs'](i[N(0x130)], {
+    'children': [i['jsx'](v, {}), i['jsx'](z, {}), i['jsx'](A, {}), i[N(0x14a)](t, {})]
+}));
+j['displayName'] = N(0x14b);
+export {j as default};
